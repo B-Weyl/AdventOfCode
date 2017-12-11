357d4d7b7dc0b540ee7e0d9e5667835ca0d0241c
B-Weyl <btw2292@gmail.com>
2017-12-11 18:19:28 -0500

    Completed 7 and WIP on 8

--- Day2/sol.py
+++ Day2/sol.py
@@ -7,24 +7,10 @@ import re
 
 
 def solve(s):
-    supports = []
-    anchors = []
-    other_values = []
-
-    for line in s:
-        new_line = line.strip().split()
-        supports.append(new_line[0])
-        if len(line.strip().split()) > 2:
-            anchors.append(new_line[0])
-            other_values.append(new_line[3:])
-
-
-    x = list(itertools.chain.from_iterable(other_values))
-    print(x)
-    for anchor in anchors:
-        if str(anchor) not in x:
-            print(anchor)
-            
+    print(s)
+    values = [re.sub('[\t\n+]', ' ', line).strip() for line in s]
+    new_values = [value.split(' ') for value in values]
+    print(new_values)
 
 
 def main():
---
+++ Day8/part1.py
@@ -0,0 +1,18 @@
+from collections import *
+import itertools
+import random
+import sys
+from math import ceil
+import re
+
+
+def solve(s):
+    for line in s:
+
+    return
+
+
+def main():
+    s = sys.stdin.readlines()
+    print(solve(s))
+main()
