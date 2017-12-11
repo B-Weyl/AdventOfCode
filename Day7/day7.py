fa9102ce7de969f80b33347093fa8c72a4973db8
B-Weyl <btw2292@gmail.com>
2017-12-11 14:22:20 -0500

    Slight changes to Day7 Part 1 soln.

--- Day7/day7.py
+++ Day7/day7.py
@@ -7,23 +7,21 @@ import re
 
 
 def solve(s):
-    supports = []
     anchors = []
     other_values = []
 
     for line in s:
         new_line = line.strip().split()
-        supports.append(new_line[0])
         if len(line.strip().split()) > 2:
             anchors.append(new_line[0])
             other_values.append(new_line[3:])
-
     x = list(itertools.chain.from_iterable(other_values))
-    # print(x)
     for anchor in anchors:
         if anchor not in x:
+            # This is ugly, but things aren't 
+            # cooperating as I would like so ugly it is:
             if anchor + ',' not in x:
-                print(anchor)        
+                print(anchor)
                 print(anchor + ',')
 
 
